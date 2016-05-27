ace86305f52e010026879517021f9a50c1e791c2
David <david_algas@hotmail.com>
2016-05-27 16:53:08 +0200

    Modified Main

--- app/src/main/java/com/example/david/calculadoraprimos/Principal.java
+++ app/src/main/java/com/example/david/calculadoraprimos/Principal.java
@@ -2,46 +2,39 @@ package com.example.david.calculadoraprimos;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class Principal extends AppCompatActivity implements View.OnClickListener {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class Principal extends AppCompatActivity {
 
     //Referencia a los widgets añadidos
-    Button btncalcular;
+    @BindView(R.id.txtNum)
     EditText txtnumero;
+
+    @BindView(R.id.areaTexto)
     TextView area;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-
-        btncalcular = (Button) findViewById(R.id.btnCalcular);
-
-        if (btncalcular != null) {
-            btncalcular.setOnClickListener(this);
-        }
+        ButterKnife.bind(this);
     }
 
-    public void onClick(View view) {
+    @OnClick(R.id.btnCalcular)
+    public void calculamos() {
 
-        //Responde al evento de pulsar el boton
-        txtnumero = (EditText) findViewById(R.id.txtNum);
-        area = (TextView) findViewById(R.id.areaTexto);
-        int num;
-
-
-        if (txtnumero.getText().toString().trim().length() == 0) {
-
+        if (txtnumero.getText().toString().isEmpty()) {
             //Comprobamos que no este vacio el EditText
-            Toast.makeText(Principal.this, "Introduzca Valor Numérico", Toast.LENGTH_SHORT).show();
+            txtnumero.setError("Rellene el Campo");
         } else {
-            num = Integer.parseInt(txtnumero.getText().toString());
+            int num = Integer.parseInt(txtnumero.getText().toString());
             area.setText(Calcular(num));
         }
     }
@@ -68,13 +61,12 @@ public class Principal extends AppCompatActivity implements View.OnClickListener
             resultado = "El Primo Numero " + numero + " es el " + ultimoPrimo;
         } else {
             if (numero == 0) {
-                Toast.makeText(Principal.this, "Introduzca Valor mayor a 0", Toast.LENGTH_SHORT).show();
+                txtnumero.setError("Valor > 0");
             }
             if (numero == 1) {
                 resultado = "El Primo Numero " + numero + " es el 1";
             }
         }
-
         return resultado;
     }
 }
